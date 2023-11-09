019afe8c06d0cbb59c1b92e2c8a6b21094c9fa01
saroj-mali <sarojmali6090@gmail.com>
2023-11-09 23:29:19 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+ const express=require('express');
+ const app=express();
+
+ require('dotenv').config();
+
+ app.use(express.json());
+
+ const blog=require("./routes/blog");
+
+ app.use("/api/v1",blog);
+
+ const connectWithDB=require("./config/database");
+
+ connectWithDB();
+
+ app.get("/",(req,res)=>{
+    res.send(`<h3>Namaste Node.js, Express.js and MongoDB 🙏</h3>`)
+ })
+
+ app.listen(8000,()=>{
+    console.log("Server is started on 8000")
+ })
---
+++ config/database.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+require("dotenv").config();
+
+const dbConnect = () => {
+  mongoose
+    .connect(process.env.DATABASE_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(console.log("DB connected "))
+    .catch((error) => {
+      console.log("DB Issue");
+      console.log(error);
+      process.exit(1);
+    });
+};
+
+
+module.exports=dbConnect;

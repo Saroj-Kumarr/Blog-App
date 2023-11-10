b4fffad2a028892bf3c8d950c6edb7cdab13e581
saroj-mali <sarojmali6090@gmail.com>
2023-11-10 23:50:07 +0530

    Just add some content

--- models/likeModel.js
+++ models/likeModel.js
@@ -17,3 +17,46 @@ const likeSchema = new mongoose.Schema({
 
 // Export 
 module.exports = mongoose.model("Like",likeSchema)
+
+
+
+// Import Mongoose 
+const mongoose = require('mongoose')
+
+
+// Route Handler 
+const likeSchema = new mongoose.Schema({
+    post:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref : "Post" //reference to the post model
+    },
+    user: {
+        type:String,
+        required:true,
+    },    
+})
+
+
+// Export 
+module.exports = mongoose.model("Like",likeSchema)
+
+
+// Import Mongoose 
+const mongoose = require('mongoose')
+
+
+// Route Handler 
+const likeSchema = new mongoose.Schema({
+    post:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref : "Post" //reference to the post model
+    },
+    user: {
+        type:String,
+        required:true,
+    },    
+})
+
+
+// Export 
+module.exports = mongoose.model("Like",likeSchema)

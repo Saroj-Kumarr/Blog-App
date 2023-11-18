619de36a94eda16f0204d43a84fa976d7548493b
saroj-mali <sarojmali6090@gmail.com>
2023-11-18 23:39:41 +0530

    Just for timepass

--- models/likeModel.js
+++ models/likeModel.js
@@ -70,6 +70,66 @@ const likeSchema = new mongoose.Schema({
 })
 
 
+const likeSchema = new mongoose.Schema({
+    post:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref : "Post" //reference to the post model
+    },
+    user: {
+        type:String,
+        required:true,
+    },    
+})
+
+
+const likeSchema = new mongoose.Schema({
+    post:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref : "Post" //reference to the post model
+    },
+    user: {
+        type:String,
+        required:true,
+    },    
+})
+
+
+const likeSchema = new mongoose.Schema({
+    post:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref : "Post" //reference to the post model
+    },
+    user: {
+        type:String,
+        required:true,
+    },    
+})
+
+
+const likeSchema = new mongoose.Schema({
+    pos
+    
+    const likeSchema = new mongoose.Schema({
+        post:{
+            type:mongoose.Schema.Types.ObjectId,
+            ref : "Post" //reference to the post model
+        },
+        user: {
+            type:String,
+            required:true,
+        },    
+    })
+    t:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref : "Post" //reference to the post model
+    },
+    user: {
+        type:String,
+        required:true,
+    },    
+})
+
+
 // Export 
 module.exports = mongoose.model("Like",likeSchema)
 
